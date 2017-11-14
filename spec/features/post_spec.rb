aad0f079cfa3b8d70960b0a300eb8e0f591db97f
Aleksandar Golubovic <agolubovicnn@gmail.com>
2017-11-14 16:36:31 +0100

    Implemented index view with posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,28 +1,36 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  before do
+    user = User.create(email: "test@test.com", password: "genije76", password_confirmation: "genije76", first_name: "Alex", last_name: "Golub")
+    login_as(user, :scope => :user)
+  end
+
   describe 'index' do
-    # before do
-    #   user = User.create(email: "test@test.com", password: "genije76", password_confirmation: "genije76", first_name: "Alex", last_name: "Golub")
-    #   login_as(user, :scope => :user)
-    #   visit posts_path
-    # end
-    it 'can be reached successfully' do
+    before do
       visit posts_path
+    end
+    it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
-    it 'has title of Posts' do
-      visit posts_path
+
+    it 'has a list of Posts' do
       expect(page).to have_content(/Posts/)
+    end
+
+    it 'has title of Posts' do
+      post1 = Post.create(date: Date.today, rationale: "Post1")
+      post2 = Post.create(date: Date.today, rationale: "Post2")
+      visit posts_path
+      expect(page).to have_content(/Post1|Post2/)
     end
   end
 
   describe 'creation' do
     before do
-      user = User.create(email: "test@test.com", password: "genije76", password_confirmation: "genije76", first_name: "Alex", last_name: "Golub")
-      login_as(user, :scope => :user)
       visit new_post_path
     end
+
     it 'has new form that can be reached' do
       expect(page.status_code).to eq(200)
     end
